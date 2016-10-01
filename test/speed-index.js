e76645c223f9b432af96467e8e67afca04239f20
Paul Irish <paul.irish@gmail.com>
2016-10-01 16:27:11 -0700

    add assertion for SI calculation from progress data.

--- test/speed-index.js
+++ test/speed-index.js
@@ -91,3 +91,58 @@ test('speed indexes calculated for 3 frame (blank @1s, content @2s, more content
 	t.is(Math.floor(indexes.perceptualSpeedIndex), 2066);
 });
 
+test('speed index starts summing from first paint', async t => {
+
+	const mockData = getMockProgessFrames();
+	/**
+	 *  We've generated frames with this visual progress curve (0, 0, 25, 75, 100)
+	 *
+	 *      ___________________________________________________
+	 *  100 |░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████████
+	 *   75 |░░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄▄▄████████
+	 *   50 |░░░░░░░░░░░░░░░░░░░░░░░░███████████████
+	 *   25 |░░░░░░░░░░░░░░░░▄▄▄▄▄▄▄▄███████████████
+	 *    0 |░░░░░░░░░░░░░░░░███████████████████████
+	 *      ----------|----------|---------|---------|---------
+	 *      0         1000      2000      3000      4000
+	 *
+	 *	Speed Index is the area above the curve. Second by second:
+	 *         1000    +  1000   +   750  +   250   +    0
+	 *  Our total sum, and thus our Speed Index, is 3000.
+	 */
+
+	const indexes = speedIndex.calculateSpeedIndexes(mockData.frames, mockData.data);
+
+	t.is(Math.floor(indexes.speedIndex), 3000);
+	t.is(Math.floor(indexes.perceptualSpeedIndex), 3000);
+});
+
+function getMockProgessFrames() {
+
+	// first two frames show no movement
+	const f0 = frame.create(null, 0);
+	const f1 = frame.create(null, 1000);
+	f0.setPerceptualProgress(0);
+	f1.setPerceptualProgress(0);
+	f0.setProgress(0);
+	f1.setProgress(0);
+	// f2 at 2s is first paint  (25% vc)
+	const f2 = frame.create(null, 2000);
+	f2.setPerceptualProgress(25);
+	f2.setProgress(25);
+	// f3 at 3s fleshes it out (75% vc)
+	const f3 = frame.create(null, 3000);
+	f3.setPerceptualProgress(75);
+	f3.setProgress(75);
+	// f4 at 4s completes things (100% vc)
+	const f4 = frame.create(null, 4000);
+	f4.setPerceptualProgress(100);
+	f4.setProgress(100);
+
+	const frames = [f0, f1, f2, f3, f4];
+	const data = { startTs: 0 };
+	return {
+		frames,
+		data
+	}
+}
