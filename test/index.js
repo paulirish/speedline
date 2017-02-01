0f1564e6e69c5029918f7d27cbcba7eed56c1dce
Paul Irish <paulirish@google.com>
2017-02-01 19:19:00 +0000

    tests for index.js

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ import speedline from '../';
 
 const TIMELINE_PATH = path.join(__dirname, 'assets/progressive-app.json');
 
-test('speedline return object should contain timming informations', async t => {
+test('speedline return object should contain timing informations', async t => {
 	const results = await speedline(TIMELINE_PATH);
 	t.is(typeof results.first, 'number');
 	t.is(typeof results.complete, 'number');
@@ -14,6 +14,13 @@ test('speedline return object should contain timming informations', async t => {
 test('speedline return object should contain speed index', async t => {
 	const results = await speedline(TIMELINE_PATH);
 	t.is(typeof results.speedIndex, 'number');
+	t.is(Math.floor(results.speedIndex), 1133);
+});
+
+test('speedline can takes timeOrigin option and adjusts results', async t => {
+	const results = await speedline(TIMELINE_PATH, {timeOrigin: 103205446186});
+	t.is(typeof results.speedIndex, 'number');
+	t.is(Math.floor(results.speedIndex), 604);
 });
 
 test('speedline return object should contain frames informations', async t => {
