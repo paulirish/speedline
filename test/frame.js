cfb0a9f6ff1ed0c4f3256935f526ce3bb6bf5b00
Paul Irish <paul.irish@gmail.com>
2017-11-15 14:08:13 -0800

    fix new test failure because of asset path change in 02038ab80f

--- test/frame.js
+++ test/frame.js
@@ -28,7 +28,7 @@ test('getHistogram should not take into account white pixels', async t => {
 });
 
 test('getHistogram should take into account brightly colored pixels', async t => {
-	const imgBuff = fs.readFileSync('./assets/rainbow.jpg');
+	const imgBuff = fs.readFileSync('./test/assets/rainbow.jpg');
 	const res = await frame.create(imgBuff, DEFAULT_TS).getHistogram();
 
 	for (const x of res) {
