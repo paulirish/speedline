4b3f1aedfa8bc6bb88f1fc2a9edf288d04fd209d
Paul Irish <paul.irish@gmail.com>
2018-08-07 12:20:37 -0700

    update paths

--- core/test/speed-index.test.js
+++ core/test/speed-index.test.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import test from 'ava';
 
-import frame from '../src/frame';
-import speedIndex from '../src/speed-index';
+import frame from '../lib/frame';
+import speedIndex from '../lib/speed-index';
 
 function calculateVisualProgressFromImages(images = [], delay = 1000) {
 	const startTs = new Date().getTime();
